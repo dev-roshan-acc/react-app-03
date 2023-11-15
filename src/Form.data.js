6c47ff7037b9e09e8a2824663403f21b69b866bf
Roshan Shrestha <acc.roshanshrestha@gmail.com>
2023-11-15 12:17:27 +0000

    first init

---
+++ src/Form.data.js
@@ -0,0 +1,20 @@
+export const formItems =() =>{
+    return  [
+        {
+          name: "firstname",
+          title: "First Name",
+          type: "text",
+        },
+        {
+          name: "email",
+          title: "Email",
+          type: "email",
+        },
+        {
+          name: "age",
+          title: "Age",
+          type: "number",
+        },
+      ];
+}
+
---
+++ src/ProductList.model.ts
@@ -0,0 +1,4 @@
+export interface ProductList {
+    brand:string,
+    category:string
+}
